fe81696873c4c8b22e4dc259179136ddee286125
Anirban <anirbanbhandari1@gmail.com>
2023-02-06 10:33:32 +0530

    Data type
    Data types example

--- src/day02/Program01.java
+++ src/day02/Program01.java
@@ -1,9 +1,25 @@
 package day02;
 
 public class Program01 {
+	
+	public static void datatype() {
+		
+		boolean b = true;
+		int a = 100;
+		double d = 107.15; 
+		char c = 'Z';
+		System.out.println(b);
+		System.out.println(a);
+		System.out.println(d);
+		System.out.println(c);
+		
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("This is the main method");
+		System.out.println("Following method is datatype():");
+		datatype();
 
 	}
 
