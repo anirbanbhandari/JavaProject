850728c205316956c330a6e110c9fa5c0f123111
Anirban <anirbanbhandari1@gmail.com>
2023-02-07 09:32:09 +0530

    Sub-Class
    Sub-Class having all the non-static methods

---
+++ src/day03/test01/Program01.java
@@ -0,0 +1,17 @@
+package day03.test01;
+
+public class Program01 {
+	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
+	public void sub(int a, int b) {
+		System.out.println(a-b);
+	}
+	public void mul(int a, int b) {
+		System.out.println(a*b);
+	}
+	public void div(int a, int b) {
+		System.out.println(a/b);
+	}
+}
