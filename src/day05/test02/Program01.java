799c7690254edb6aef15154335d47d66edd28b2c
Anirban <anirbanbhandari1@gmail.com>
2023-02-10 09:08:37 +0530

    Multi level inheritance
    Multi level inheritance code

---
+++ src/day05/test02/Program01.java
@@ -0,0 +1,13 @@
+package day05.test02;
+
+public class Program01 {
+	
+	public static void display1() {
+		System.out.println("Good morning!");
+	}
+	
+	public void displayNonStatic1() {
+		System.out.println("Good morning non-static!");
+	}
+
+}
---
+++ src/day05/test02/Program02.java
@@ -0,0 +1,13 @@
+package day05.test02;
+
+public class Program02 extends Program01{
+	
+	public static void display2() {
+		System.out.println("Good day!");
+	}
+	
+	public void displayNonStatic2() {
+		System.out.println("Good day non-static!");
+	}
+
+}
