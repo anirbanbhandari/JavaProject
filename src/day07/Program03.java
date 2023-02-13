43fa30b30700db73337d68c0823daad469de62cd
Anirban <anirbanbhandari1@gmail.com>
2023-02-13 08:36:31 +0530

    Day07
    Even-Odd number

---
+++ src/day07/Program03.java
@@ -0,0 +1,28 @@
+package day07;
+
+import java.util.Scanner;
+
+public class Program03 {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter the number:- ");
+		int a=x.nextInt();
+		x.close();
+		
+		
+		System.out.println("Start Program...");
+		
+		if(a%2==0)
+		{
+			System.out.println("a is even...!");
+		}
+		else {
+			System.out.println("a is odd...!");
+		}
+		
+		System.out.println("End Program...");
+		
+	}
+
+}
