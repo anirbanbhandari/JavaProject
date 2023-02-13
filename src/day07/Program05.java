06008d8d0049d76a495573d215231cc0f135cdc2
Anirban <anirbanbhandari1@gmail.com>
2023-02-13 09:13:28 +0530

    Day07
    Comparing a,b,c

---
+++ src/day07/Program05.java
@@ -0,0 +1,37 @@
+package day07;
+
+import java.util.Scanner;
+
+public class Program05 {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+		System.out.println("Enter the first number:- ");
+		int a=x.nextInt();
+		System.out.println("Enter the second number:- ");
+		int b=x.nextInt();
+		System.out.println("Enter the third number:- ");
+		int c=x.nextInt();
+		x.close();
+		
+		
+		System.out.println("Start Program...");
+		
+		if((a>b) && (a>c))
+		{
+			System.out.println("First number is the greatest...!");
+		}
+		else if((b>a) && (b>c)) 
+		{
+			System.out.println("Second number is the greatest...!");
+		}
+		else 
+		{
+			System.out.println("Third number is the greatest...!");
+		}
+		
+		System.out.println("End Program...");
+		
+	}
+
+}
