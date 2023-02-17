a67780297eba4137e5a942a16e087d29753ca3bd
Anirban <anirbanbhandari1@gmail.com>
2023-02-17 08:46:50 +0530

    Day10
    1. Do-while
    2. For loop
    3. Table formation

---
+++ src/day10/Program02.java
@@ -0,0 +1,18 @@
+package day10;
+
+public class Program02 {
+
+	public static void main(String[] args) {
+		
+		for(int a=1; a<=5; a++) 
+		{
+			System.out.println("Good day...!");
+		}
+		
+		for(int b=-1; b>=-5; b=b-1)
+		{
+			System.out.println("Good morning...!");
+		}
+	}
+
+}
