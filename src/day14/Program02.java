e140861890bdfe344681bc333550329f62f0b7fc
Anirban <anirbanbhandari1@gmail.com>
2023-02-28 09:01:24 +0530

    Day14
    Comment update

--- src/day14/Program02.java
+++ src/day14/Program02.java
@@ -8,12 +8,12 @@ public class Program02 {
 
 		for(int x:x1)
 		{
-			String y = String.valueOf(x);
+			String y = String.valueOf(x); //converting integer values into string to read the number one by one
 			int count = y.length();			
 			double z=0;
 			for(int i=0;i<count;i++)
 			{
-				z=Math.pow(Integer.parseInt(y.charAt(i)+""), count)+z;
+				z=Math.pow(Integer.parseInt(y.charAt(i)+""), count)+z; //parse the string value as a signed decimal value
 			}
 			if(z==x)
 			{
