bb5776ae19be055a2bf93e69595829d4731a6a99
Anirban <anirbanbhandari1@gmail.com>
2023-03-03 14:40:30 +0530

    Day15
    Comment update

--- src/day15/Program02.java
+++ src/day15/Program02.java
@@ -12,17 +12,17 @@ public class Program02 {
 		x.add(30);
 		x.add(20);
 		x.add(40);
-		System.out.println(x);
+		//System.out.println(x);
 	
 		x.add(10);
 		x.add(30);
 		x.add(20);
 		x.add(40);
-		System.out.println(x);
+		//System.out.println(x);
 		
 		
 		x.remove(10);
-		System.out.println(x);
+		//System.out.println(x);
 		
 			
 		Integer[] y= {30,50,60,10,70,90,80,30};
